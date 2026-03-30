06151ad4a1ac90c747926831b3a2891aca77545d
Jonas Bak Phillipson <jbakchr@gmail.com>
2026-03-30 12:06:58 +0200

    Added root project build

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,24 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 
-export default defineConfig({
-  root: "examples/dev", // 👈 This enables vite dev
-  build: {
-    lib: {
-      entry: resolve(__dirname, "src/index.ts"),
-      name: "kaplayFit",
-      fileName: "kaplayFit",
-    },
-  },
-  plugins: [dts({ rollupTypes: true })],
+export default defineConfig(({ command }) => {
+  if (command === "serve") {
+    // `vite dev`
+    return {
+      root: "examples/dev",
+    };
+  } else {
+    // `vite build`
+    return {
+      build: {
+        lib: {
+          entry: resolve(__dirname, "src/index.ts"),
+          name: "kaplayFit",
+          fileName: "kaplayFit",
+        },
+        outDir: "dist", // 👈 output to project root
+      },
+      plugins: [dts({ rollupTypes: true })],
+    };
+  }
 });

6e60709988fec32ffa951c052155b9c2bf01d7a2
Jonas Bak Phillipson <jbakchr@gmail.com>
2026-03-30 14:51:50 +0200

    Improved release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,22 +2,23 @@
 set -e
 
 # --- Color definitions ---
-GREEN="\033[0;32m"
-CYAN="\033[0;36m"
+GREEN="\033[1;32m"
+CYAN="\033[1;36m"
 YELLOW="\033[1;33m"
 RED="\033[0;31m"
 RESET="\033[0m"
 
-echo -e "${CYAN}Bumping version…${RESET}"
+
+echo -e "\n${CYAN}Bumping version…${RESET}\n"
 npm version patch
 
-echo -e "${CYAN}Building…${RESET}"
+echo -e "\n${CYAN}Building…${RESET}\n"
 npm run build
 
-echo -e "${CYAN}Pushing to Git…${RESET}"
+echo -e "\n${CYAN}Pushing to Git…${RESET}\n"
 git push
 
-echo -e "${CYAN}Publishing to npm…${RESET}"
+echo -e "\n${CYAN}Publishing to npm…${RESET}\n"
 npm publish
 
-echo -e "${GREEN}Done!${RESET}"
+echo -e "\n${GREEN}Done!${RESET}\n"
